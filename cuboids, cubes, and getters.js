95f75f283e7aa10cda321d89ee97732f31b7b41a
TJ <xxterrelljacksonxx@gmail.com>
2022-07-26 10:59:23 -0400

    solutions

--- cuboids, cubes, and getters.js
+++ cuboids, cubes, and getters.js
@@ -21,3 +21,25 @@ class Cuboid {
       this.height = length
     }
   }
+
+  class Cuboid {
+    constructor(length, width, height){
+      this.length = length
+      this.width = width
+      this.height = height
+    }
+    get volume() {
+      let {length, width, height} = this
+      return length * width * height
+    }
+    get surfaceArea() {
+      let {length, width, height} = this
+      return 2 * (length * width + length * height + height * width)
+    }
+  }
+  
+  class Cube extends Cuboid {
+    constructor(length){
+      super(length, length, length)
+    }
+  }
