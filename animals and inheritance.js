0726d034b284484b1848d6d865cceaeffe2176d7
TJ <xxterrelljacksonxx@gmail.com>
2022-07-29 09:48:11 -0400

    solutions

---
+++ animals and inheritance.js
@@ -0,0 +1,34 @@
+// Get Coding :)
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+      super(name, age, status);
+      this.legs = 0;
+      this.species = 'shark';
+      this.status = status
+    }
+  }
+  
+  class Cat extends Animal {
+    constructor(name, age, status){
+      super(name, age, status)
+      this.legs = 4
+      this.species = 'cat'
+      this.status = status
+    }
+    introduce() {
+      return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, age, status, master){
+      super(name, age, status)
+      this.legs = 4
+      this.species = 'dog'
+      this.status = status
+      this.master = master
+    }
+    greetMaster() {
+      return `Hello ${this.master}`
+    }
