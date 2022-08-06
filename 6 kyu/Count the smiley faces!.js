f8cc8f206af3bd46bbae1b29e4e594459dc73022
TJ <xxterrelljacksonxx@gmail.com>
2022-08-06 17:36:45 -0400

    solution

--- 6 kyu/Count the smiley faces!.js
+++ 6 kyu/Count the smiley faces!.js
@@ -1,10 +1,11 @@
 function countSmileys(arr) {
     let count = 0
-    for(let face of arr){
-      let re = /[:;][-~]?[)D]/g
-      if(face.match(re))
-        count ++
+    for (let face of arr) {
+        let re = /[:;][-~]?[)D]/g
+        if (face.match(re))
+            count++
     }
     return count
-  }
-  
+}
+
+const countSmileys = arr => arr.filter(face => face.match(/[:;][-~]?[)D]/g)).length
