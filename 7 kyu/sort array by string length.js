365d871d6f3bde6947cc145f285ae1b64f27d9c3
TJ <xxterrelljacksonxx@gmail.com>
2022-08-20 11:42:20 -0400

    js update

--- 7 kyu/sort array by string length.js
+++ 7 kyu/sort array by string length.js
@@ -2,4 +2,7 @@ function sortByLength(array) {
     return array.sort((a, b) => a.length - b.length, 0)
 };
 
-const sortByLength = array => array.sort((a, b) => a.length - b.length, 0)    
+const sortByLength = array => array.sort((a, b) => a.length - b.length, 0)
+
+
+
---
+++ algorithms and data structures/map, filter, reduce.js
@@ -0,0 +1,23 @@
+let students = [
+    { name: 'Max', id: 31, score: 80 },
+    { name: 'Kate', id: 15, score: 69 },
+    { name: 'Treyvon', id: 16, score: 35 },
+    { name: 'Lisa', id: 7, score: 55 }
+]
+
+// let names = []
+// for (let i = 0; i < students.length; i++){
+//     names.push(students[i].name.toUpperCase())
+// }
+
+// const names = students.map((obj) => obj.name.toUpperCase())
+
+// let overSixty = []
+// for (let i = 0; i < students.length; i++){
+//     if (students[i].score > 60) overSixty.push(students[i].name)
+// }
+
+// const overSixty = students.filter((student) => student.score > 60 && student.id > 15).map(student => student.name)
+
+// let sum = students.reduce((acc, curr) => acc + curr.score, 0)
+
