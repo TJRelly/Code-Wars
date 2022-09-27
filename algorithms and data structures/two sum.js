0869ca18af3b15775db978ad0db6d56b1624d472
TJ <xxterrelljacksonxx@gmail.com>
2022-09-27 19:34:31 -0400

    algorithms

--- algorithms and data structures/two sum.js
+++ algorithms and data structures/two sum.js
@@ -1,15 +1,26 @@
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     let map = {}
-    for (let i = 0; i < nums.length; i++){
+    for (let i = 0; i < nums.length; i++) {
         map[nums[i]] = i
     }
-    console.log (map)
+    console.log(map)
 
-    for (let j = 0; j < nums.length; j++){
+    for (let j = 0; j < nums.length; j++) {
         let curr = nums[j]
         let comp = target - curr
-        if (map[comp] && map[comp] !== j){
+        if (map[comp] && map[comp] !== j) {
             return [map[comp], j]
         }
+    }
+};
+
+const twoSum = (nums, target) => {
+    let map = {}
+    for (let i = 0; i < nums.length; i++) map[nums[i]] = i
+
+    for (let j = 0; j < nums.length; j++) {
+        let curr = nums[j]
+        let comp = target - curr
+        if (map[comp] && map[comp] !== j) return [map[comp], j]
     }
 };
