8ee5c96c5c8f946a287a3bc98fcc90ce73665e55
TJ <xxterrelljacksonxx@gmail.com>
2022-09-28 19:14:46 -0400

    algorithms

---
+++ 7 kyu/breaking chocolate problem.js
@@ -0,0 +1,11 @@
+function breakChocolate(n, m) {
+    numOfBreaks = m * n - 1
+    if (n === 0 || m === 0) numOfBreaks = 0
+    return numOfBreaks
+}
+
+function breakChocolate(n, m) {
+    return (n === 0 || m === 0)
+        ? numOfBreaks = 0
+        : numOfBreaks = m * n - 1
+}
