56c7330e208360415b046ca8236cc12421be18ca
TJ <xxterrelljacksonxx@gmail.com>
2022-10-25 08:43:09 -0400

    solution

---
+++ 7 kyu/find the stray number.js
@@ -0,0 +1,17 @@
+//find the number that is different in the array
+//[1,1,1,2] //2
+//Is the list sorted? 
+//are the lists the same length?
+
+//function that takes in arrays
+//sorts the items
+//if the first two are equal return the last num
+//esle return the first num
+
+function stray(numbers) {
+    numbers.sort((a, b) => a - b)
+    if (numbers[0] === numbers[1]) return numbers.at(-1)
+    else return (numbers[0])
+  }
+
+  
