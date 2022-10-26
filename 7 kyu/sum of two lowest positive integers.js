f97dc35d270e3b7b47887924460e1cc1adc30f00
TJ <xxterrelljacksonxx@gmail.com>
2022-10-26 08:31:09 -0400

    solution

--- 7 kyu/sum of two lowest positive integers.js
+++ 7 kyu/sum of two lowest positive integers.js
@@ -1,4 +1,9 @@
 function sumTwoSmallestNumbers(numbers) {  
-    numbers.sort((a, b)=> a -b)
+    numbers.sort((a, b)=> a - b)
     return numbers[0] + numbers[1]
+  }
+
+  const sumTwoSmallestNumbers = nums => {
+    nums.sort((a, b) => a - b)
+    return nums[0] + nums[1]
   }
