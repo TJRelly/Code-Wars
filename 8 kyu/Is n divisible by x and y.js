0caaadcedb98a29650bb5a516ed115abc65e37d8
TJ <xxterrelljacksonxx@gmail.com>
2022-11-01 17:43:49 -0400

    "solution"

--- 8 kyu/Is n divisible by x and y.js
+++ 8 kyu/Is n divisible by x and y.js
@@ -1,1 +1,7 @@
 const isDivisible = (n, x, y) => n % x === 0 && n % y === 0
+
+const isDivisible = (n, x, y) => !(n % x) && !(n % y)
+
+function isDivisible(n, x, y) {
+    return !(n % x) && !(n % y)
+  }
