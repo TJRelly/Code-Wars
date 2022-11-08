33d0fac5bf3fac1a970e9a03b2d083777133a488
TJ <xxterrelljacksonxx@gmail.com>
2022-11-08 17:08:20 -0500

    solution

--- 7 kyu/odd or even.js
+++ 7 kyu/odd or even.js
@@ -6,3 +6,6 @@ function oddOrEven(array) {
     let sum = array.reduce((acc, curr) => acc + curr, 0)
     return !(sum % 2) ? 'even' : 'odd'
 }
+
+const oddOrEven = (array) => !(array.reduce((acc, curr) => acc + curr, 0) % 2) ? 'even' : 'odd'
+
