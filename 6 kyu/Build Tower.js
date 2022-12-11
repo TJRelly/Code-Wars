7b1bcc61acc4bbbec174a79d7d5b139760611d66
TJ <xxterrelljacksonxx@gmail.com>
2022-12-11 09:24:32 -0500

    solution

--- 6 kyu/Build Tower.js
+++ 6 kyu/Build Tower.js
@@ -1,11 +1,23 @@
 function towerBuilder(nFloors) {
-    let space, star, tower = []
-    for(let i = 0; i < nFloors; i++){
-      space = ' '.repeat(nFloors - i - 1)
-      star = '*'.repeat(i * 2 + 1)
-      tower.push(`${space}${star}${space}`)
-    }
-    return tower
+  let space, star, tower = []
+  for (let i = 0; i < nFloors; i++) {
+    space = ' '.repeat(nFloors - i - 1)
+    star = '*'.repeat(i * 2 + 1)
+    tower.push(`${space}${star}${space}`)
   }
+  return tower
+}
 
-  console.log(towerBuilder(10))
+console.log(towerBuilder(8))
+
+towerBuilder2 = nFloors => {
+  let space, star, tower = []
+  for (let i = 0; i < nFloors; i++) {
+    space = ' '.repeat(nFloors - i - 1)
+    star = '*'.repeat(i * 2 + 1)
+    tower.push(`${space}${star}${space}`)
+  }
+  return tower
+}
+
+console.log(towerBuilder2(12))
