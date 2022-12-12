d997614c348392f2a824b22515d972b2257fa8d9
TJ <xxterrelljacksonxx@gmail.com>
2022-12-12 09:26:04 -0500

    solution

--- 6 kyu/Convert string to camel case.js
+++ 6 kyu/Convert string to camel case.js
@@ -2,7 +2,8 @@ function toCamelCase(str){
     if (str.includes('_') || str.includes('-')){
       let cleanStr = str.replace(/[_-]/g, ' ')
       let words = cleanStr.split(' ')
-      return words.map((word, i) => i > 0 
+      return words.map((word, i) => 
+      i > 0 
                        ? word.charAt(0)
                        .toUpperCase() 
                        + words[i].slice(1) 
