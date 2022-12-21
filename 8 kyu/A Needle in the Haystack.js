82bc71dcaaf057af9566e87356a533b245b6d85b
TJ <xxterrelljacksonxx@gmail.com>
2022-12-21 16:30:59 -0500

    solution

--- 8 kyu/A Needle in the Haystack.js
+++ 8 kyu/A Needle in the Haystack.js
@@ -2,3 +2,7 @@ function findNeedle(haystack) {
     let index = haystack.indexOf('needle')
     return `found the needle at position ${index}`
   }
+
+  function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`
+  }
