d212b93ecd1ef53fb3a9778c6bc9731c81fe9e0b
TJ <xxterrelljacksonxx@gmail.com>
2023-01-04 11:12:18 -0500

    solution

--- 6 kyu/find the missing letter.js
+++ 6 kyu/find the missing letter.js
@@ -1,10 +1,21 @@
 array1 = [ 'a', 'b', 'c', 'd', 'f' ] // 'e'
 array2 = ['O','Q','R','S'] // 'P'
 
+//map the array
+//change each value to a number
+//check if nums are consecutive with loop
+//return the missing number and index
+//splice the missing letter
+
 function findMissingLetter(array)
 {
-  array.sort()
   console.log(array)
+  let nums = array.map(e => e.charCodeAt(0) - 96)
+  console.log(nums)
+  let consecutive = nums.forEach((num, i) =>{
+    num[i] === num[i + 1] - 1 ? '' : num[i] + 1
+  })
+  console.log(consecutive)
 }
 
 findMissingLetter(array1)
