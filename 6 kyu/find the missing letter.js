d901b52be3972df96e5096bf4402556edb60470f
TJ <xxterrelljacksonxx@gmail.com>
2023-01-04 10:59:38 -0500

    solution

---
+++ 6 kyu/find the missing letter.js
@@ -0,0 +1,12 @@
+array1 = [ 'a', 'b', 'c', 'd', 'f' ] // 'e'
+array2 = ['O','Q','R','S'] // 'P'
+
+function findMissingLetter(array)
+{
+  array.sort()
+  console.log(array)
+}
+
+findMissingLetter(array1)
+
+findMissingLetter(array2)
