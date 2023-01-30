5f0bbbe938b680261feb0f64f483e65f25ee5f4c
TJ <xxterrelljacksonxx@gmail.com>
2023-01-30 17:03:32 -0500

    solution

--- 7 kyu/the highest profit wins!.js
+++ 7 kyu/the highest profit wins!.js
@@ -9,3 +9,9 @@ function minMax(arr){
     let max = Math.max(...arr)
     return [min, max]
   }
+
+  const minMax = arr => {
+    let min = Math.min(...arr)
+    let max = Math.max(...arr)
+    return [Math.min(...arr), Math.max(...arr)]
+  }
