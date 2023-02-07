7d3e89dcc9fd0c39c17f4b970d14aceb9b890a39
TJ <xxterrelljacksonxx@gmail.com>
2023-02-07 12:53:16 -0500

    solution

--- 6 kyu/find the missing letter.js
+++ 6 kyu/find the missing letter.js
@@ -7,20 +7,31 @@ array2 = ['O', 'Q', 'R', 'S'] // 'P'
 //return the missing number and index
 //splice the missing letter
 
-function findMissingLetter1(arr){
-    for(let i = 0; i < arr.length; i++){
+function findMissingLetter1(arr) {
+    for (let i = 0; i < arr.length; i++) {
         let curr = arr[i].charCodeAt(0)
         let next = arr[i + 1].charCodeAt(0)
-        if(next - curr > 1) return String.fromCharCode(curr + 1)
+        if (next - curr > 1) return String.fromCharCode(curr + 1)
     }
 }
-// const findMissingLetter = arr => {
-//     for (let i = 0; i < arr.length; i++){
-//       let curr = arr[i].charCodeAt(0)
-//       let next = arr[i + 1].charCodeAt(0)
-//       if (next - curr > 1) return String.fromCharCode(curr + 1)
-//     }
-//   }
+
+const findMissingLetter2 = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        let curr = arr[i].charCodeAt(0)
+        let next = arr[i + 1].charCodeAt(0)
+        if (next - curr > 1) return String.fromCharCode(curr + 1)
+    }
+}
+
+const findMissingLetter = arr => {
+    arr.reduce((acc, curr, i), arr[curr])
+}
+
+function findMissingLetter4(array) {
+    var i = array[0].charCodeAt();
+    array.map(x => x.charCodeAt() == i ? i++ : i);
+    return String.fromCharCode(i);
+}
 
 console.log(findMissingLetter(array1))
 
