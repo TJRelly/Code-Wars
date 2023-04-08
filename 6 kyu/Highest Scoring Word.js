3f0e8c31e84a271f9e55b602eb72905a5572558d
TJ <xxterrelljacksonxx@gmail.com>
2023-04-07 21:27:45 -0400

    solution

--- 6 kyu/Highest Scoring Word.js
+++ 6 kyu/Highest Scoring Word.js
@@ -7,3 +7,10 @@ function high(x){
     , 0))
     return arr[score.indexOf(Math.max(...score))]
   }
+
+//2nd solution after break
+  const high = (str) => {
+    let words = str.split(' ')
+    let score = words.map(word => [...word].reduce((a, c) => a + c.charCodeAt(0) - 96, 0))
+    return words[score.indexOf(Math.max(...score))]
+  }
