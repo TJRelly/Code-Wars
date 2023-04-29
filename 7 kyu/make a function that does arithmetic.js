37c0daf5c59803476faf84bc28c8327c335a6388
TJ <xxterrelljacksonxx@gmail.com>
2023-04-29 09:09:14 -0400

    solution

--- 7 kyu/make a function that does arithmetic.js
+++ 7 kyu/make a function that does arithmetic.js
@@ -14,3 +14,12 @@ function arithmetic(a, b, operator) {
       break
   }
 }
+
+function arithmetic(a, b, operator) {
+  switch(operator){
+    case 'add': return a + b; break
+    case 'subtract': return a - b; break
+    case 'multiply': return a * b; break
+    case 'divide': return a / b; break
+  }
+}
