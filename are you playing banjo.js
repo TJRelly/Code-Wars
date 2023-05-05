6b6f65add71920458d058f86c15970dd0b5139df
TJ <xxterrelljacksonxx@gmail.com>
2023-05-05 08:48:09 -0400

    solution

--- are you playing banjo.js
+++ are you playing banjo.js
@@ -1,9 +1,11 @@
 function areYouPlayingBanjo(name) {
     first = name.toLowerCase()[0]
     return first === 'r' ? name + " plays banjo" : name + " does not play banjo"
-  }
+}
 
-  function areYouPlayingBanjo(name) {
+const areYouPlayingBanjo = (name) => {
     first = name.toLowerCase()[0]
-    return first === 'r' ? name + " plays banjo" : name + " does not play banjo"
-  }
+    return first === 'r'
+        ? name + " plays banjo"
+        : name + " does not play banjo"
+}
