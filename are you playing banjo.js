7a1fc83b690aff2ffbced2c6712c78e3d7a61a2d
TJ <xxterrelljacksonxx@gmail.com>
2023-05-07 08:46:22 -0400

    solution

---
+++ are you playing banjo.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+    first = name.toLowerCase()[0]
+    return first === 'r' ? name + " plays banjo" : name + " does not play banjo"
+  }
+
+  function areYouPlayingBanjo(name) {
+    first = name.toLowerCase()[0]
+    return first === 'r' ? name + " plays banjo" : name + " does not play banjo"
+  }
