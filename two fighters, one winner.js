f2c20619cb522819e5bc48911033ae71dfe73b30
TJ <xxterrelljacksonxx@gmail.com>
2023-05-19 17:14:21 -0400

    solution

--- two fighters, one winner.js
+++ two fighters, one winner.js
@@ -5,3 +5,9 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     else if (damage2 > damage1) return fighter2.name
     else return firstAttacker
 }
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    let hitpoints1 = Math.ceil(fighter1.health/fighter2.damagePerAttack)
+    let hitpoints2 = Math.ceil(fighter2.health/fighter1.damagePerAttack)
+    return hitpoints1 > hitpoints2 ? fighter1.name : hitpoints2 > hitpoints1 ? fighter2.name : firstAttacker
+  }
