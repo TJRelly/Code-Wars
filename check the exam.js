8d0b68de46478f22793d14f70235696f5f828db6
TJ <xxterrelljacksonxx@gmail.com>
2023-06-05 17:31:13 -0400

    solution

--- check the exam.js
+++ check the exam.js
@@ -7,3 +7,15 @@ function checkExam(array1, array2) {
     }
     return score > 0 ? score : 0
   }
+
+  function checkExam(array1, array2) {
+    let key = array1
+    let res = array2.reduce((score, answer, i) => {
+      if (answer === key[i]) score += 4
+      else if (answer === '') score += 0
+      else score -= 1
+      console.log(answer, key[i], score)
+      return score
+   }, 0)
+   return res > 0 ? res : 0
+  }
