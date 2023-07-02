a4123d8e7553d0ae0572fede3d37b8c313b2aefc
TJ <xxterrelljacksonxx@gmail.com>
2023-07-02 00:13:46 -0400

    used Set object

---
+++ vowel count.js
@@ -0,0 +1,30 @@
+function getCount(str) {
+    let count = 0
+    const vowels = ['a', 'e','i','o','u']
+    for (let letter of str) {
+      if (vowels.includes(letter)) count ++
+    }
+    return count
+  }
+
+  function getCount(str) {
+    const vowels = ['a', 'e','i','o','u']
+    return [...str].reduce((a, c) => {
+      if (vowels.includes(c)) a++
+      return a
+    }, 0)
+  }
+
+  function getCount(str) {
+    let count = 0
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    for (let letter of str) {
+      if (vowels.has(letter)) count ++
+    }
+    return count
+  }
+
+  function getCount(str) {
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    return [...str].filter(letter => vowels.has(letter)).length
+  }
