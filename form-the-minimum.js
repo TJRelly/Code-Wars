d1781e2d4453b48c53cd84f8415d42d7075f590f
TJ <xxterrelljacksonxx@gmail.com>
2023-07-28 16:39:56 -0400

    includes, sort

--- form-the-minimum.js
+++ form-the-minimum.js
@@ -3,11 +3,20 @@
 // Input >> Output Examples
 // minValue ({1, 3, 1})  ==> return (13)
 
-function minValue(values){
-    values.sort((a, b) => a - b)
-    let unique = []
-    for (let value of values) {
-      if (!unique.includes(value)) unique.push(value)
-    }
-    return Number(unique.join(""))
+function minValue(values) {
+  values.sort((a, b) => a - b)
+  let unique = []
+  for (let value of values) {
+    if (!unique.includes(value)) unique.push(value)
   }
+  return Number(unique.join(""))
+}
+
+function minValue(values) {
+  return Number(
+    values
+      .sort((a, b) => a - b)
+      .filter((value, i, arr) => arr.indexOf(value) === i)
+      .join("")
+  )
+}
