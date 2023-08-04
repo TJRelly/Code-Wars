6a77e5521bc7ac095ff70dca8a44ffc08723d31b
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-08-04 16:20:41 -0400

    loop

--- are these in order.js
+++ are these in order.js
@@ -13,6 +13,13 @@ inAscOrder([1,6,10,18,2,4,20]); // returns false
 inAscOrder([9,8,7,6,5,4,3,2,1]); // retturns false
 
 function inAscOrder(arr) {
+    for (let i = 0; i < arr.length - 1; i ++) {
+      if(arr[i] > arr[i + 1]) return false
+    }
+    return true
+  }
+
+function inAscOrder(arr) {
     //copy arr, sort arr, compare arr
     return arr.slice()
       .sort((a, b) => a - b)
