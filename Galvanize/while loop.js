549b565bc40511e39aa4e2995f737ac5176002b4
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-10-05 23:38:13 -0400

    for loop

--- Galvanize/while loop.js
+++ Galvanize/while loop.js
@@ -45,3 +45,26 @@ loopASequenceAgain(3, 7)
 // 5
 // 6
 // 7
+
+function loopAnArrayAgain(array) {
+  // create a loop which iterates over the input array
+  // log current array element to the console
+  for (item of array) {
+    console.log(item)
+  }
+}
+
+loopAnArrayAgain(["a", "b", "c", "d"])
+// console output:
+// a
+// b
+// c
+// d
+
+loopAnArrayAgain([1, 2, 3, 4, 5])
+// console output:
+// 1
+// 2
+// 3
+// 4
+// 5
