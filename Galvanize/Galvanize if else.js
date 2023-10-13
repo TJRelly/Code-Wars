17ab562b5be919c6fa10b4c5e572f5c2c2b9488c
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-10-13 11:25:45 -0400

    if else objects spelling

--- Galvanize/Galvanize if else.js
+++ Galvanize/Galvanize if else.js
@@ -22,3 +22,31 @@ function findIngredient(ingredientList, ingredientToSearchFor) {
   }
 
 console.log(findIngredient(bakingList, ingredient))
+
+function measureRequiredFruit(fruitTotals) {
+  // if there are more than 3 bananas and more than 10 strawberries
+    // return 'We have enough fruit, with {totalBananas} bananas, and {totalStrawberries} strawberries'
+    let bananas = fruitTotals["bananas"]
+    console.log(bananas)
+    let strawberries = fruitTotals["stawberries"]
+    console.log(strawberries)
+    if (bananas > 3 && strawberries > 10) return "We have enough fruit , with " + bananas + " bananas, and " + strawberries + " strawberries"
+  // otherwise
+    // return 'We do not have enough of both fruits'
+    else return "We do not have enough of both fruits"
+}
+
+var result1 = measureRequiredFruit({bananas: 4, strawberries: 12});
+console.log('should log "We have enough fruit, with 4 bananas, and 12 strawberries":', result1);
+
+var result2 = measureRequiredFruit({bananas: 5, strawberries: 15});
+console.log('should log "We have enough fruit, with 5 bananas, and 15 strawberries":', result2);
+
+var result3 = measureRequiredFruit({bananas: 2, strawberries: 12});
+console.log('should log "We do not have enough of both fruits":', result3);
+
+var result4 = measureRequiredFruit({bananas: 5, strawberries: 8});
+console.log('should log "We do not have enough of both fruits":', result4);
+
+var result5 = measureRequiredFruit({bananas: 3, strawberries: 9});
+console.log('should log "We do not have enough of both fruits":', result5);
