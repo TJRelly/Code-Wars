776beb970d957c2e54bf33d32f5ee618c0dc1322
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-11-08 20:09:51 -0500

    recursion

--- Galvanize Basic Prep/alphabet war.js
+++ Galvanize Basic Prep/alphabet war.js
@@ -20,21 +20,24 @@
 // The other letters don't have power and are only victims.
 
 function alphabetWar(fight) {
-  
-    let left = 0;
-    let right = 0;
-    
-    for(let i = 0; i < fight.length; i ++) {
-      if(fight[i] === "w") left += 4;
-      if(fight[i] === "p") left += 3;
-      if(fight[i] === "b") left += 2;
-      if(fight[i] === "s") left += 1;
-      
-      if(fight[i] === "m") right += 4;
-      if(fight[i] === "q") right += 3;
-      if(fight[i] === "d") right += 2;
-      if(fight[i] === "z") right += 1;
-    }
-    
-    return left > right ? "Left side wins!" : right > left ? "Right side wins!" : "Let's fight again!";
+  let left = 0
+  let right = 0
+
+  for (let i = 0; i < fight.length; i++) {
+    if (fight[i] === "w") left += 4
+    if (fight[i] === "p") left += 3
+    if (fight[i] === "b") left += 2
+    if (fight[i] === "s") left += 1
+
+    if (fight[i] === "m") right += 4
+    if (fight[i] === "q") right += 3
+    if (fight[i] === "d") right += 2
+    if (fight[i] === "z") right += 1
   }
+
+  return left > right
+    ? "Left side wins!"
+    : right > left
+    ? "Right side wins!"
+    : "Let's fight again!"
+}
--- Galvanize Basic Prep/factorial.js
+++ Galvanize Basic Prep/factorial.js
@@ -1,6 +1,29 @@
-function computeFactorialOfN(n) {
+// Recursive Function - a function that calls itself from within
+// It help you to visualize a complex problem in smaller repeatable steps
+
+// Create a recursive function
+
+// 2 Parts
+// Base Case - stops function
+// Recursive call - holds the repeatable steps
+
+// Example - Factorial - product of all the positive integers between a number and 1
+// 4! = 4 * 3 * 2 * 1
+
+function factorial(n) {
+    //base case
     if(n === 0) return 1
-    return n * computeFactorialOfN(n - 1)
+    //recursive call
+    return n * factorial(n - 1)
 }
 
-console.log(computeFactorialOfN(6))
+console.log(factorial(4))
+
+4 * 6
+3 * 2
+2 * 1
+1 * 1
+
+numerator = 12
+denominator = 0
+fraction = denominator/ numerator
