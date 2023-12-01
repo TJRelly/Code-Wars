487dcd935cf47744fbf767057499224d028cba8c
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-11-30 19:53:49 -0500

    union

--- CSX/callbacks and higher order functions/objOfMatches.js
+++ CSX/callbacks and higher order functions/objOfMatches.js
@@ -5,19 +5,46 @@
 
 //? reduce
 
-function objOfMatches(arr1, arr2, callback) {
-    return arr1.reduce((obj, curr, i) => {
-      //take each current value and run through callback function
-      //compare result of invocation to corresponding item in array 2
-      //if match array 1 item = key and array 2 item = value
-      if(arr2[i] === callback(curr)) obj[curr] = arr2[i]
-      return obj
-    }, {})
-  }
-  
+// function objOfMatches(arr1, arr2, callback) {
+//     return arr1.reduce((obj, curr, i) => {
+//       //take each current value and run through callback function
+//       //compare result of invocation to corresponding item in array 2
+//       //if match array 1 item = key and array 2 item = value
+//       if(arr2[i] === callback(curr)) obj[curr] = arr2[i]
+//       return obj
+//     }, {})
+//   }
+
+//declare function
+//input: arr1, arr2, cb
+//output: obj
+function objOfMatches(arr1, arr2, cb) {
+//   //acc: obj
+//   const acc = {}
+//   //curr: array
+//   //iterate first array
+//   for (let i = 0; i < arr1.length; i++) {
+//     //cb each element
+//     let result = cb(arr1[i])
+//     //if the evaluated result matches corr element in arr2
+//     if (result === arr2[i]) {
+//       //add arr1 element as key
+//       //and arr2 element as value
+//       acc[arr1[i]] = arr2[i]
+//     }
+//   }
+
+//   return acc
+return arr1.reduce((obj, curr, i) => {
+    if(cb(curr) === arr2[i]) obj[curr] = arr2[i]
+    return obj
+  }, {})
+}
 
 // Uncomment these to check your work!
-// const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
-// const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
-// function uppercaser(str) { return str.toUpperCase(); }
-// console.log(objOfMatches(arr1, arr2, uppercaser)); // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
+const arr1 = ["hi", "howdy", "bye", "later", "hello"]
+const arr2 = ["HI", "Howdy", "BYE", "later", "HELLO"]
+function uppercaser(str) {
+  return str.toUpperCase()
+}
+console.log(objOfMatches(arr1, arr2, uppercaser)) // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
