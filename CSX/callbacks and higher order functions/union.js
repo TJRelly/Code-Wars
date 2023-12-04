78e9f050cc5dd51f96fac8fe351774a616ab35cf
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-12-04 16:19:29 -0500

    union

--- CSX/callbacks and higher order functions/union.js
+++ CSX/callbacks and higher order functions/union.js
@@ -1,4 +1,4 @@
-// function union(arrays){	
+// function union(arrays){
 //   // let result = []
 //   // for(let arr of arrays) {
 //   //     for(let num of arr) {
@@ -17,22 +17,22 @@
 //declare union array
 //input: array of arrays
 //output: array
-function union(arrays){
-    //reduce
-    return arrays.reduce((acc, curr) => {
-      //iterate curr array
-      //if acc includes element don't push
-      //push element to acc)
-      curr.forEach(element => {
-        if(!acc.includes(element)) acc.push(element)
-      })
-        //return acc
-      return acc
+function union(arrays) {
+  //reduce
+  return arrays.reduce((acc, curr) => {
+    //iterate curr array
+    //if acc includes element don't push
+    //push element to acc)
+    curr.forEach((element) => {
+      if (!acc.includes(element)) acc.push(element)
     })
-  }
-  
-  // Uncomment these to check your work!
-  const arr1 = [5, 10, 15];
-  const arr2 = [15, 88, 1, 5, 7];
-  const arr3 = [100, 15, 10, 1, 5];
-  console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
+    //return acc
+    return acc
+  })
+}
+
+// Uncomment these to check your work!
+const arr1 = [5, 10, 15]
+const arr2 = [15, 88, 1, 5, 7]
+const arr3 = [100, 15, 10, 1, 5]
+console.log(union([arr1, arr2, arr3])) // should log: [5, 10, 15, 88, 1, 7, 100]
