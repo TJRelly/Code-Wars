30589f8c8c21c793717f8e16cc9a4aa4b2395811
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2023-12-05 16:18:11 -0500

    reduce, forEach

--- CSX/callbacks and higher order functions/union.js
+++ CSX/callbacks and higher order functions/union.js
@@ -1,17 +1,34 @@
-function union(arrays){	
-    // let result = []
-    // for(let arr of arrays) {
-    //     for(let num of arr) {
-    //       if(!result.includes(num)) result.push(num)
-    //     }
-    //   }
-    // return result
-    return arrays.reduce((acc, arr) => {
-      for(let item of arr) {
-        if (!acc.includes(item)) acc.push(item)
-      }
+// function union(arrays){	
+//   // let result = []
+//   // for(let arr of arrays) {
+//   //     for(let num of arr) {
+//   //       if(!result.includes(num)) result.push(num)
+//   //     }
+//   //   }
+//   // return result
+//   return arrays.reduce((acc, arr) => {
+//     for(let item of arr) {
+//       if (!acc.includes(item)) acc.push(item)
+//     }
+//     return acc
+//   }, [])
+// }
+
+//declare union array
+//input: array of arrays
+//output: array
+function union(arrays){
+    //reduce
+    return arrays.reduce((acc, curr) => {
+      //iterate curr array
+      //if acc includes element don't push
+      //push element to acc)
+      curr.forEach(element => {
+        if(!acc.includes(element)) acc.push(element)
+      })
+        //return acc
       return acc
-    }, [])
+    })
   }
   
   // Uncomment these to check your work!
