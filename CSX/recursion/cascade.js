024e1c1400664542177e5ceb83191064acfd4c33
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2024-01-11 14:52:41 -0500

    async

--- CSX/recursion/cascade.js
+++ CSX/recursion/cascade.js
@@ -1,13 +1,32 @@
-function cascade(number, placeHolder) {
-    if(number === 1) {
-        console.log(number)
-        console.log(placeHolder)
-        return 1
-    }
+function cascade(number, originalNum, increasing = false) {
+    // if(number === 1) {
+    //     console.log(number)
+    //     console.log(placeHolder)
+    //     return 1
+    // }
 
-    placeHolder = number
+    // placeHolder = number
+    // console.log(number)
+    // return cascade(Math.floor(number/10), placeHolder)
+    originalNum = number
+    number = number.toString()
+    
+    console.log(originalNum)
+    //base case 1: if number is equal to original number and increasing is true
+        //return 
+    if(number === originalNum && increasing) return    
+    //if not increasing //log number    
+    //if number length is equal to 1 then increasing is equal to true
+    if(number.length === 1) increasing = true
+    //if increasing use original number to restore each digit    
     console.log(number)
-    return cascade(Math.floor(number/10), placeHolder)
+    //recursive case 1: cut digit from number
+    if(!increasing) return cascade(number.slice(0, -1), originalNum, increasing)
+    //recursive case 2: add digit back to number from originalNum
+    else {
+        number += originalNum[0]
+        return cascade(number, originalNum.slice(1), increasing)
+    }
 }
 
 console.log(cascade(12345))
---
+++ async vs. syncronous course/test.js
@@ -0,0 +1,4 @@
+const array = [];
+array.push('first');
+setTimeout(() => array.push('second'), 2000);
+array.push('third');
