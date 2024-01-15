86d49f90f2b9f4e2ed9d06aab310dcc97ad9db47
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2024-01-15 08:15:37 +0000

    hash map

--- async vs. syncronous course/stringManipulation.js
+++ async vs. syncronous course/stringManipulation.js
@@ -6,23 +6,24 @@ function removeDups(str){
     // //declare result 
     // let result = ""
     // //iterate array
-    // for(const letter of arr){
-    //     //check if duplicate
-    //     let isDup = false
-    //     for(const char of result) {
-    //         if(letter === char) isDup = true
-    //     }
-    //     //if not duplicate concat with result
-    //     if(!isDup) result += letter
+    // for(const letter of str){
+    //     // //check if duplicate
+    //     // let isDup = false
+    //     // for(const char of result) {
+    //     //     if(letter === char) isDup = true
+    //     // }
+    //     // //if not duplicate concat with result
+    //     // if(!isDup) result += letter
+    //     //using includes
+    //     if(!result.includes(letter)) result += letter
     // }   
     // //return result
     // return result
 
     //save letter to hash map
-    
     const map = {}
     str.split("").forEach(letter => {
-        map[letter] = ++map[letter] || 1
+        map[letter] = true
     })
     //return keys
     return Object.keys(map).join("")
