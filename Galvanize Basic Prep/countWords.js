f72899d5e665a1f4f7c16bf9165c22840a1e88cf
Terrell Jackson <94482664+TJRelly@users.noreply.github.com>
2024-02-01 11:40:26 -0500

    split, reduce

---
+++ Galvanize Basic Prep/countWords.js
@@ -0,0 +1,17 @@
+// Write a function called "countWords".
+
+// Given a string (words separated by spaces), "countWords" returns an object where each key is a word in the given string, with its value being how many times that word appeared in the given string. If given an empty string, your function should return an empty object.
+
+function countWords(stringOfWords) {
+  if (!stringOfWords.length) return {};
+  return stringOfWords.split(" ").reduce((obj, word) => {
+    obj[word] = ++obj[word] || 1;
+    return obj;
+  }, {});
+}
+
+let result1 = countWords("ask a bunch get a bunch");
+console.log('should log "{ask: 1, a: 2, bunch: 2, get: 1}":', result1);
+
+let result2 = countWords("");
+console.log('should log "{}":', result2);
